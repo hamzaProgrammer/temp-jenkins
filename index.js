0ee9eddf35f445bf56425aff78618909f4b3a444
Hamza Maqsood <hmaqsood295@gmail.com>
2023-12-25 00:32:52 +0500

    sec commit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const app = express()
 const PORT = 5000
 
-app.get("/",(req,res)=>{
-    res.send("welcome to jenkins")
+app.get("/", (req, res) => {
+    res.send("welcome to jenkins 11")
 })
 
-app.listen(PORT,()=>{
-    console.log("app is running on ",PORT)
+app.listen(PORT, () => {
+    console.log("app is running on ", PORT)
 })
