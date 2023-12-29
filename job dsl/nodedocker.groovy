38a0a9f3cc0294dea9292a414dd17b2d091ba661
Hamza Maqsood <hmaqsood295@gmail.com>
2023-12-29 23:57:11 +0500

    changes

--- job dsl/nodedocker.groovy
+++ job dsl/nodedocker.groovy
@@ -9,12 +9,12 @@ job('node js project ver 02'){
     wrappers {
         nodejs('Node 16')
         credentialsBinding{
-            usernamePassword('hamza78674', 'hamza78674' )
+            usernamePassword('hamza78674', 'hamza_78674' )
         }
     }
     steps{
         shell('npm install')
-        shell('docker login -u hamza78674 -p hamza78674')
+        shell('docker login -u hamza78674 -p hamza_78674')
         dockerBuildAndPublish {
             repositoryName('hamza78674/nodejs-jenkins-demo')
             tag('${BUILD_NUMBER}')
